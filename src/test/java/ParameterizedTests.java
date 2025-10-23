8424a14cac89ba2d42b0f7f04285a0ef38c6329f
Tatiana Andreeva <andretat8@gmail.com>
2025-10-23 18:52:57 +0200

    Comments from Nikita

--- src/test/java/ParameterizedTests.java
+++ src/test/java/ParameterizedTests.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import data.Language;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -8,19 +7,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.Selenide.*;
+
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ParameterizedTests {
     @BeforeAll
-    static void beforeAll() {
+    static void setupEnvironment() {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
-//      Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
     }
-
-    Selenide selenide = new Selenide();
 
     @ValueSource(strings = {
             "standard_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"
@@ -29,7 +28,8 @@ public class ParameterizedTests {
     void logInWorksForAllUsers(String userValue) {
         String siteValue = "https://www.saucedemo.com/";
         String passwordValue = "secret_sauce";
-        selenide.open(siteValue);
+
+        open(siteValue);
         $("#user-name").setValue(userValue);
         $("#password").setValue(passwordValue);
         $("#login-button").click();
@@ -42,7 +42,7 @@ public class ParameterizedTests {
     @EnumSource(Language.class)
     @DisplayName("Сайт отображает языки корректно")
     void sushiSiteShouldShowCorrectTextDependsOnLang(Language language) {
-        selenide.open("https://yumesushi.rs/");
+        open("https://yumesushi.rs/");
         $("header select#language").selectOptionByValue(language.value);
         $("div.MuiGrid2-root.MuiGrid2-container.MuiGrid2-direction-xs-row.MuiGrid2-spacing-xs-1.burger-category__grid.css-1rkz8o4").shouldHave(text(language.description));
 
@@ -55,7 +55,7 @@ public class ParameterizedTests {
     })
     @ParameterizedTest(name = "Для каждого user {0} заполняется форма")
     void eachFieldIsFilledInTextBox(String name, String email, String currentAddress, String permanentAddress) {
-        selenide.open("https://demoqa.com/text-box");
+        open("https://demoqa.com/text-box");
         $("#userName").setValue(name);
         $("#userEmail").setValue(email);
         $("#currentAddress").setValue(currentAddress);
